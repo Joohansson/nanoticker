ab5c15305b54980fb09d49f1b2a4b22bcca7ec8d
Joohansson <d84@outlook.com>
2019-08-25 22:18:08 +0200

    Support blockCount as string

--- script/calc-reps.py
+++ script/calc-reps.py
@@ -14,6 +14,9 @@ import requests
 import re
 import logging
 
+#Own references
+import repList
+
 """CUSTOM VARS"""
 BETA = True #SET TO False FOR MAIN NET
 
@@ -51,111 +54,10 @@ peerInfo = [] #connected peers with weight info
 confCountLimit = 1000 #lower limit for block count to include confirmation average
 confSpanLimit = 60000 #lower limit for time span to include confirmation average
 
-#Default starting node list. Is dynamically updated
-repsInitB = ['https://beta.api.nanocrawler.cc',
-    'https://monitor-beta.mynano.ninja',
-    'http://beta.warai.me',
-    'http://benna.just-dmitry.ru',
-    'https://beta.nanoskynode.com',
-    'https://b.repnode.org',
-    'https://nano-faucet.org/beta/monitor',
-    'http://beta.nanode.cc',
-    'https://json.nanoticker.info',
-    'http://95.216.205.225',
-    'http://kamikaze.awiki.org/betaNanoNodeMonitor',
-    'http://173.249.54.87:8080'
-    ]
-
-repsInitM = [
-    'https://nano.nifni.net',
-    'https://repnode.org',
-    'https://nano.voting',
-    'https://dbachm123-nano.mynanorep.com',
-    'https://warai.me',
-    'http://polishnanonode.cc',
-    'http://185.243.8.228',
-    'https://nonna.just-dmitry.ru',
-    'http://173.249.54.87',
-    'https://moomoo.cryptohell.io',
-    'http://node.wean.de',
-    'http://3dpenis.io',
-    'http://node.fastfeeless.com',
-    'http://95.216.206.138',
-    'https://nano2.strnmn.me',
-    'https://blankslate.tk',
-    'http://nanode.cc',
-    'http://oflipstaro.247node.com',
-    'http://rep.nanoisfast.com',
-    'http://arainode.com',
-    'http://139.59.181.118',
-    'https://monitor.nanolinks.info',
-    'https://nano-rep.xyz',
-    'http://bbdevelopment.website',
-    'https://node.nanoes.info',
-    'https://nano.ganknodes.online',
-    'http://nanotipbot.com/nanoNodeMonitor',
-    'http://vmi220922.contaboserver.net',
-    'http://nano.nodegasm.com',
-    'http://159.65.95.42',
-    'http://hronanos.duckdns.org',
-    'https://srvicentehd.info/nano',
-    'http://nano-node-01.scheler.io',
-    'http://nanode.blizznerds.com/nano',
-    'https://node.nanoble.org',
-    'https://nanomakonode.com',
-    'https://monitor.mynano.ninja',
-    'http://50.99.115.219:8080',
-    'https://lightnano.rocks',
-    'http://104.131.79.207',
-    'http://188.166.18.100',
-    'https://nano.mehl.no',
-    'https://nanoodle.io/nanoNodeMonitor',
-    'http://antennano.ch',
-    'http://167.86.102.128',
-    'https://brainblocks.io/monitor',
-    'http://37.235.52.37/nanoNodeMonitor',
-    'http://46.101.114.167',
-    'http://80.241.211.21',
-    'http://81.169.174.237',
-    'http://95.216.228.244/monitor',
-    'http://98.26.20.44/monitor',
-    'http://104.131.169.14',
-    'http://134.0.113.170',
-    'http://146.198.58.202',
-    'http://148.251.12.252',
-    'http://159.89.125.101',
-    'http://159.203.117.254',
-    'http://163.172.69.5',
-    'http://167.71.111.204/nanoNodeMonitor',
-    'http://172.83.15.108/nanoNodeMonitor',
-    'http://174.107.247.162',
-    'http://193.31.24.222',
-    'http://206.189.18.189',
-    'http://206.189.20.214',
-    'https://node.mansour.io',
-    'https://nano.muffin-mafia.de',
-    'https://yapraiwallet.space/nanoNodeMonitor',
-    'https://brainblocks.io/monitor/nw',
-    'https://node.nanovault.io',
-    'https://brainblocks.io/monitor/nm',
-    'http://node.nanologin.com',
-    'https://www.freenanofaucet.com/nanoNodeMonitor',
-    'https://node.nano.lat',
-    'http://numsu.tk:13080',
-    'https://nanoslo.0x.no',
-    'http://dangilsystem.zapto.org/nanoNodeMonitor',
-    'http://51.15.62.124',
-    'http://120.27.224.224',
-    'http://134.209.61.219',
-    'http://159.89.112.19',
-    'http://159.89.146.74',
-    'https://www.nanoskynode.com',
-]
-
 if BETA:
-    repsInit = repsInitB
+    repsInit = repList.repsInitB
 else:
-    repsInit = repsInitM
+    repsInit = repList.repsInitM
 
 #Excluded from any result
 blacklist = []
@@ -192,7 +94,7 @@ async def getMonitor(url):
             r = await fetch(session, url)
             try:
                 rjson = json.loads(r)
-                if rjson['currentBlock'] > 0:
+                if int(rjson['currentBlock']) > 0:
                     return [rjson, True, url, r]
             except:
                 return [{}, False, url, r]
@@ -204,10 +106,10 @@ async def verifyMonitor(url):
         async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
             r = await fetch(session, url)
             try:
-                r = json.loads(r)
-                if r['currentBlock'] > 0:
+                rjson = json.loads(r)
+                if int(rjson['currentBlock']) > 0:
                     url = url.replace('/api.php','')
-                    return [r['nanoNodeAccount'], url]
+                    return [rjson['nanoNodeAccount'], url]
             except:
                 pass
     except:
@@ -315,9 +217,8 @@ async def getAPI():
                 try:
                     if task.result() is not None and task.result():
                         if (task.result()[1]):
-                            if task.result()[0]['currentBlock'] > 0:
-                                jsonData.append(task.result()[0])
-                                #log.info(timeLog('Valid: ' + task.result()[0]['nanoNodeName']))
+                            jsonData.append(task.result()[0])
+                            #log.info(timeLog('Valid: ' + task.result()[0]['nanoNodeName']))
                         else:
                             log.warning(timeLog('Could not read json from %s. Result: %s' %(task.result()[2], task.result()[3])))
 
@@ -415,7 +316,7 @@ async def getAPI():
                     pass
 
                 try:
-                    weight = j['votingWeight']
+                    weight = int(j['votingWeight'])
                     if (weight >= latestOnlineWeight*0.001):
                         PRStatus = True
                     else:
@@ -1056,6 +957,7 @@ async def getPeers():
 
         #Update the final list
         reps = validPaths.copy()
+        log.info(reps)
 
         await peerSleep(startTime)
 
